5f5c475be2ae84f04bcf3327672554931286fee7
Fred Biam <fredb@ecole-centrale.ma>
2026-03-09 11:25:32 +0000

    changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -52,8 +52,8 @@ app.use(
     credentials: true,
   })
 )
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: "2mb" }))
+app.use(express.urlencoded({ extended: true, limit: "2mb" }))
 app.use(cookieParser())
 app.use((req, res, next) => {
   const start = Date.now()
